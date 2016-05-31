a7f714c8bd2024ab07a991efde7634c36dbaa50c
Jere Paavola <jere.paavola@gmail.com>
2016-05-31 18:09:26 +0300

    using rsplit and limiting to only one split

--- photo_organizer.py
+++ photo_organizer.py
@@ -149,10 +149,10 @@ class PhotoOrganizer(object):
         """
         
         base, ext = os.path.splitext(filename)
-        if len(base.split('_')) == 1:
+        if len(base.rsplit('_', 1)) == 1:
             number = 1
         else:
-            number = int(base.split('_')[1]) + 1
+            number = int(rsplit('_', 1)[1]) + 1
         
         return base + '_' + str(number) + ext                
                 
